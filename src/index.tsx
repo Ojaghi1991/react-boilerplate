706806e8161304f0ef3a719c3092234c3207f93f
Hussein Ojaghi <h.ojaghi@roundtableapps.com>
2021-02-20 11:40:10 +0330

    use FC in clean ways

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { FC } from "react";
 import ReactDom from "react-dom";
 
-const App: React.FC = () => <div> Start Boilerplate </div>;
+const App: FC = () => <div> Start Boilerplate </div>;
 
 ReactDom.render(<App />, document.getElementById("root"));
