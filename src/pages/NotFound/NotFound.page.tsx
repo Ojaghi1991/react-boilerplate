27065fbd1b04e4a1ca86994a6a0b03ce59153e42
Hussein Ojaghi <h.ojaghi@roundtableapps.com>
2021-03-03 17:08:15 +0330

    add Dashboard and Not Found page

---
+++ src/pages/NotFound/NotFound.page.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+const NotFound: FC = () => (<div>Page Not Found</div>);
+
+export default NotFound;
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+import Dashboard from './Dashboard';
+import NotFound from './NotFound';
+
+export default { Dashboard, NotFound };
