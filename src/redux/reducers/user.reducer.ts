8819bf8b3ae6850cd3e626a2e4ec5ed8863f959f
Hussein <HusseinOjaghi1991@gmail.com>
2021-06-11 23:23:59 +0430

    Add reducer
    add sample reducer to use in component

---
+++ src/redux/reducers/user.reducer.ts
@@ -0,0 +1,35 @@
+import ActionTypes from "../actionTypes";
+
+export const initialState = {
+  fetching: undefined,
+};
+
+export default (state = initialState, action:any) => {
+  switch (action.type) {
+     /**
+     * LOAD_ALL_USER
+     */
+    case ActionTypes.LOAD_ALL_USER_REQUESTING:
+      return {
+        ...state,
+        fetching: true,
+      };
+
+    case ActionTypes.LOAD_ALL_USER_SUCCESS:
+      return {
+        ...state,
+        data: action.data,
+        fetching: false,
+      };
+
+    case ActionTypes.LOAD_ALL_USER_FAILURE:
+      return {
+        ...state,
+        error: action.error,
+        fetching: false,
+      };
+
+    default:
+      return state;
+  }
+};
