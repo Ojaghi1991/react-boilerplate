731ac16067b01c08db11ceca07294625052ea15e
Hussein <HusseinOjaghi1991@gmail.com>
2021-06-11 23:37:56 +0430

    add redux configuration both in client and server

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
 import { renderRoutes } from 'react-router-config';
 import { loadableReady } from '@loadable/component';
 
+import createStore from "../redux/store";
+
+// @ts-ignore
+const store = createStore(window.INITIAL_STATE);
 const hotModule = (module as any);
 
 const render = (routes: Array<any>) => (hotModule.hot ? ReactDOM.render : ReactDOM.hydrate)(
   <AppContainer>
-    <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
+    </Provider>
   </AppContainer>,
   document.getElementById('react-view'),
 );
--- src/server/server.tsx
+++ src/server/server.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import path from 'path';
+import { Provider } from "react-redux";
 import express from 'express';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
@@ -9,6 +10,7 @@ import { ChunkExtractor, ChunkExtractorManager } from '@loadable/server';
 import htmlContent from '../utils/htmlContent';
 import routes from '../router';
 import devServer from './devServer';
+import createStore from "../redux/store";
 
 const PORT = process.env.PORT || 3000;
 const expressApp = express();
@@ -22,12 +24,15 @@ const statsFile = path.resolve('./dist/loadable-stats.json');
 
 expressApp.get('*', (req: express.Request, res: express.Response) => {
   const extractor = new ChunkExtractor({ statsFile });
+  const store = createStore({});
 
   const component = (
     <ChunkExtractorManager extractor={extractor}>
-      <StaticRouter location={req.path} context={{}}>
-        {renderRoutes(routes)}
-      </StaticRouter>
+      <Provider store={store}>
+        <StaticRouter location={req.path} context={{}}>
+          {renderRoutes(routes)}
+        </StaticRouter>
+      </Provider>
     </ChunkExtractorManager>
   );
   const content = renderToString(component);
