cefe6c5e8c1796e941ebcd639404afe3a2f21294
Hussein Ojaghi <husseinojaghi1991@gmail.com>
2021-06-22 10:59:08 +0430

    move config to root of project

---
+++ config/common.ts
@@ -0,0 +1,6 @@
+export default {
+  dev: process.env.NODE_ENV || "development",
+  host: process.env.SERVER_HOST || "0.0.0.0",
+  port: process.env.SERVER_PORT || 3000,
+  apiURL: process.env.API_URL || "https://jsonplaceholder.typicode.com",
+};
