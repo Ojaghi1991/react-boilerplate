3f6b8956e7da7889bd3b957f03d5a1f5ea7495bf
Hussein Ojaghi <h.ojaghi@roundtableapps.com>
2021-07-04 17:09:07 +0430

    remove useless console log

--- src/redux/reducers/sample.reducer.ts
+++ src/redux/reducers/sample.reducer.ts
@@ -8,7 +8,6 @@ const cookies = new Cookies();
 export default (state = initialState, action: any) => {
   switch (action.type) {
     case ActionTypes.SAMPLE:
-      console.log("action.data", action.data);
       cookies.set("appToken", action.data);
       return {
         data: action.data,
